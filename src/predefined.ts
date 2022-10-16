0fe06c6cfbad1490fd8dd46c318f9048a9ffb314
Igor Klepacki <neg4n@icloud.com>
2022-10-16 17:36:59 +0200

    Change predefined next serializer Date conversion from toLocaleString to toISOString

--- src/predefined.ts
+++ src/predefined.ts
@@ -4,7 +4,7 @@ import { isDate, isUndefined } from 'lodash'
 export function nextServerSideSerialize(data: Record<string, unknown>) {
   return serialize(data, (unserializedData) => {
     if (isDate(unserializedData)) {
-      return unserializedData.toLocaleString()
+      return unserializedData.toISOString()
     } else if (isUndefined(unserializedData)) {
       return null
     } else if (unserializedData instanceof URL) {
